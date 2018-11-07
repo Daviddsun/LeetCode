d0fa81693e4f56c09139aec63de88d7bcb7b84e5
Wan Youwen <1756429307@qq.com>
2018-11-07 22:57:16 +0800

    给定一个数组 nums ，和一个目标值target 返回 数组中 和 为 target 的两个索引

--- 001_TwoSum/001_TwoSum.cpp
+++ 001_TwoSum/001_TwoSum.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 class Solution {
 public:
+    // 给定一个数组 nums ，和一个目标值target
+    // 返回 数组中 和 为 target 的两个索引
 	vector<int> twoSum(vector<int>& nums, int target) {
-		hash_map<int, int> m; // unordered_map
+		hash_map<int, int> m; // unordered_map  无序map  差值元素 o(1)
 		for (int i = 0; i < nums.size(); ++i){
-			if (m.count(target - nums[i])){
-				return{m[target - nums[i]], i};
+			if (m.count(target - nums[i]))// 差值 目标差值是否存在
+			{
+				return{m[target - nums[i]], i};//找到返回 两个元素的 索引 
 			}
-			m[nums[i]] = i;
+			m[nums[i]] = i;// 插入map记录 值，index
 		}
 		return{};
 	}
